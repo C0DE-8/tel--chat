65c28785c03c9483fa3cfac0556ae11ceb19e323
8amjoker404 <8amjoker@gmail.com>
2026-08-06 11:41:33 -0700

    Serve widget demo from root

--- backend/routes/root.routes.js
+++ backend/routes/root.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+});
+
+router.get("/status", (req, res) => {
   res.json({ message: "botting is runing" });
 });
 
